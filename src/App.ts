58375c9208a5c989adabe62cf6d57cf7dcfcd633
Joel Bugarini <joelbugarini@gmail.com>
2020-08-19 15:08:16 -0700

    select multiple

--- src/App.ts
+++ src/App.ts
@@ -4,9 +4,49 @@ import './App.css';
 
 class App {
   Hello: KnockoutObservable<string>;
+  Fruits: KnockoutObservable<Array<any>>;
+  FruitsSelected: KnockoutObservable<Array<any>>;
   constructor() {
     this.Hello = ko.observable("Hello, World!");
+    this.FruitsSelected = ko.observable([]);  
+    this.Fruits = ko.observable([]);
+
+    getFruits(()=>{
+      this.Fruits([
+        { name: "Apple", value: 1 },
+        { name: "Watermellon", value: 2 },
+        { name: "Orange", value: 3 },
+        { name: "Cherry", value: 4 }
+      ]);
+    });
+    
+    getFruitsSelected(() => {
+      this.FruitsSelected([
+        { name: "Apple", value: 1 }
+      ]);
+    });
+
   }
+
+  FruitAdded(){
+
+  }
+
+  FruitDeleted(){
+
+  }
+}
+
+let getFruits = (callback: any) => {
+  setTimeout(() => {
+    callback();
+  }, 4215);
+}
+
+let getFruitsSelected = (callback: any) => {
+  setTimeout(() => {
+    callback();
+  }, 2543);
 }
 
 export default App;
--- src/bindingHandlers.ts
+++ src/bindingHandlers.ts
@@ -1,15 +1,44 @@
 import * as ko from "knockout";
+import 'chosen-js';
+import 'chosen-js/chosen.css';
 
 export function bindingHandlers() {
-    ko.bindingHandlers.yourBindingName = {
+    ko.bindingHandlers.tags = {
         init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
             // This will be called when the binding is first applied to an element
             // Set up any initial state, event handlers, etc. here
+            let bindings = ko.utils.unwrapObservable(allBindings());
+  
+            $(element).chosen({
+                no_results_text: "No se encontraron elementos"
+            });
+            
+            $(element).on('change', (evt: any, params: any) => {
+                if (typeof params.selected !== "undefined") {
+                    console.log("selected", params)
+                }
+                if (typeof params.deselected !== "undefined") {
+                    console.log("deselected", params)
+                }               
+            });
+
         },
         update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
             // This will be called once when the binding is first applied to an element,
             // and again whenever any observables/computeds that are accessed change
             // Update the DOM element based on the supplied values here.
+            let bindings = ko.utils.unwrapObservable(allBindings());
+            let value = bindings.tagValue;
+            let text = bindings.tagText;
+            let tags_selected = bindings.tagsSelected().map((x: any) => x[value]);
+
+            bindings.tags().forEach((item:any) => {
+                let selected: boolean = tags_selected.includes(item[value]);
+
+                $(element).append(new Option(item[text], item[value], undefined, selected));
+            });
+
+            $(element).trigger("chosen:updated");
         }
     };
 }
